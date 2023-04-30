9b49bb6ec8caae20663aa27469844c59d0214383
Aaron <65768952+aaronkhq@users.noreply.github.com>
2023-04-30 23:38:27 +0800

    Update config_nobelium.js

--- themes/nobelium/config_nobelium.js
+++ themes/nobelium/config_nobelium.js
@@ -1,11 +1,11 @@
 const CONFIG_NOBELIUM = {
 
   // 菜单配置
-  MENU_CATEGORY: false, // 显示分类
+  MENU_CATEGORY: true, // 显示分类
   MENU_TAG: true, // 显示标签
-  MENU_ARCHIVE: false, // 显示归档
+  MENU_ARCHIVE: true, // 显示归档
   MENU_SEARCH: true, // 显示搜索
-  MENU_RSS: false, // 显示订阅
+  MENU_RSS: true, // 显示订阅
 
   NAV_NOTION_ICON: true // 是否读取Notion图标作为站点头像
 
